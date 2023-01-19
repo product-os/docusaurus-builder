ec31cd75cd1a3a6be7563d6c69b70b7176f8e00a
Alex Bucknall <alex.bucknall@gmail.com>
2023-01-19 21:39:55 +0000

    Adds support for locally building using Docker
    Change-type: minor
    Signed-off-by: Alex Bucknall <alex.bucknall@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,25 @@
 #!/bin/sh -l
 
-echo "Inputs: $1 $2 $3 $4"
-. /app/docusaurusify.sh $1 $2 $3 $4
+if [ "$DEV" = 'true' ] ; then
+    echo "Development mode is enabled"
+    export PROJECT_NAME="development build"
+    export URL="https://localhost"
+    rm /app/docs/README.md
+    cp -rf /app/dev/docs /app
+    cp -rf /app/dev/static /app/docs || true
+    cp -u /app/dev/README.md /app/docs/README.md || true
+else
+    echo "Inputs: $1 $2 $3 $4"  
+    . /app/docusaurusify.sh $1 $2 $3 $4
+fi
+
 cd /app
-npm ci
-npx docusaurus build
-cp -rf /app/build /github/workspace
+
+if [ "$DEV" = 'true' ] ; then
+    echo "Serving locally..."
+    npm run start
+else
+    # npm ci
+    npx docusaurus build
+    cp -rf /app/build /github/workspace
+fi
